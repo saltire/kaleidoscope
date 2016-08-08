150a9dae77a90ffd363c63bf927082579fbfde36
saltire sable <saltiresable@gmail.com>
2016-08-07 21:24:51 -0400

    Remove paper.js, convert to pure HTML canvas API

--- index.js
+++ index.js
@@ -1,46 +1,61 @@
 var sliceCount = 6;
 var rotateTime = 3000;
 
-paper.setup(document.getElementById('canvas'));
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
+ctx.translate(centerX, centerY);
 
-var image = new paper.Raster('./rainbow.png', paper.view.center);
+var radius = Math.min(centerX, centerY);
+var startAngle = 1.5 * Math.PI;
+var arcLength = 2 * Math.PI / sliceCount;
+var margin = .25 * Math.PI / 180;
+var sliceCanvas = document.createElement('canvas');
+sliceCanvas.width = canvas.width;
+sliceCanvas.height = canvas.height;
+sliceCtx = sliceCanvas.getContext('2d');
+sliceCtx.translate(centerX, centerY);
+sliceCtx.beginPath();
+sliceCtx.moveTo(0, 0);
+sliceCtx.arc(0, 0, radius, startAngle - arcLength / 2 - margin, startAngle + arcLength / 2 + margin, false);
+sliceCtx.lineTo(0, 0);
+sliceCtx.clip();
 
-image.onLoad = function () {
-    var circle = new paper.Path.Circle(paper.view.center, 300);
-    var mask = new paper.Path.Arc(
-        circle.getPointAt(circle.length - (circle.length / sliceCount / 2)),
-        circle.getPointAt(0),
-        circle.getPointAt(circle.length / sliceCount / 2));
-    mask.firstSegment.handleIn = new paper.Point(0, 0);
-    mask.lastSegment.handleOut = new paper.Point(0, 0);
-    mask.insert(0, paper.view.center);
-    mask.add(paper.view.center);
-    mask.clipMask = true;
+var imageCanvas = document.createElement('canvas');
 
-    var group = new paper.Group([image, mask]);
+var lastTs = 0;
+function drawFrame(ts) {
+    var delta = ts - lastTs;
+    lastTs = ts;
 
-    var slices = [];
-    for (var i = 0; i < sliceCount; i++) {
-        var slice = group.clone();
-        if (i % 2) {
-            slice.scale(1, -1);
-        }
-        slice.rotate(360 / sliceCount * i, paper.view.center);
-        slices.push(slice);
+    var rotatePercent = delta / rotateTime;
+    sliceCtx.rotate(rotatePercent * 2 * Math.PI);
+    sliceCtx.drawImage(imageCanvas, -centerX, -centerY);
+
+    for (var i = 0; i < sliceCount; i += 2) {
+        ctx.drawImage(sliceCanvas, -centerX, -centerY);
+        ctx.rotate(arcLength * 2);
     }
-
-    group.visible = false;
-
-    paper.view.onFrame = function(event) {
-        slices.forEach(function (slice, i) {
-            var rotatePercent = event.delta * 1000 / rotateTime;
-            if (i % 2) {
-                rotatePercent *= -1;
-            }
-            slice.firstChild.rotate(rotatePercent * 360, paper.view.center);
-        });
+    ctx.scale(-1, 1);
+    ctx.rotate(arcLength);
+    for (var i = 0; i < sliceCount; i += 2) {
+        ctx.drawImage(sliceCanvas, -centerX, -centerY);
+        ctx.rotate(arcLength * 2);
     }
+    ctx.scale(-1, 1);
+    ctx.rotate(arcLength);
+
+    window.requestAnimationFrame(drawFrame);
+}
+
+var image = new Image();
+image.src = './rainbow.png';
+image.onload = function () {
+    imageCanvas.width = image.width;
+    imageCanvas.height = image.height;
+    var imageCtx = imageCanvas.getContext('2d');
+    imageCtx.drawImage(image, 0, 0);
+
+    window.requestAnimationFrame(drawFrame);
 };
-
-
-
