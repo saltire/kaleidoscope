4db3dd3c2b3fcbba0a6dd65492911afdcc9c06c1
saltire sable <saltiresable@gmail.com>
2016-08-10 14:29:09 -0400

    Add some comments

--- kaleidoscope.js
+++ kaleidoscope.js
@@ -3,6 +3,7 @@
 var Kaleidoscope = function Kaleidoscope(canvasId, imagePath, points, rotateTime) {
     var _this = this;
 
+    // Set up main canvas.
     this.canvas = document.getElementById(canvasId);
     this.ctx = this.canvas.getContext('2d');
     this.centerX = this.canvas.width / 2;
@@ -11,14 +12,19 @@ var Kaleidoscope = function Kaleidoscope(canvasId, imagePath, points, rotateTime
     this.size = Math.min(this.canvas.width, this.canvas.height);
     this.radius = this.size / 2;
 
+    // Set up slice canvas.
     this.setPoints(points);
-    this.rotateTime = rotateTime;
 
-    this.imageCanvas = document.createElement('canvas');
-    this.imageCtx = this.imageCanvas.getContext('2d');
+    // Initialize rotation variables.
+    this.rotateTime = rotateTime;
     this.lastTs = 0;
     this.rotatePercent = 0;
 
+    // Set up image canvas.
+    this.imageCanvas = document.createElement('canvas');
+    this.imageCtx = this.imageCanvas.getContext('2d');
+
+    // Load image and draw it onto image canvas.
     var image = new Image();
     image.src = imagePath;
     image.onload = function () {
@@ -26,22 +32,26 @@ var Kaleidoscope = function Kaleidoscope(canvasId, imagePath, points, rotateTime
         _this.imageCanvas.height = image.height;
         _this.imageCtx.drawImage(image, 0, 0);
 
+        // Request the first animation frame.
         window.requestAnimationFrame(_this.drawFrame.bind(_this));
     };
 };
 
 Kaleidoscope.prototype.setPoints = function (points) {
-    this.points = points;
-
-    var startAngle = 1.5 * Math.PI;
-    this.arcLength = Math.PI / this.points;
-    var margin = .25 * Math.PI / 180;
-
+    // Set up slice canvas.
     this.sliceCanvas = document.createElement('canvas');
     this.sliceCanvas.width = this.size;
     this.sliceCanvas.height = this.size;
     this.sliceCtx = this.sliceCanvas.getContext('2d');
     this.sliceCtx.translate(this.radius, this.radius);
+
+    // Set slice variables.
+    this.points = points;
+    this.arcLength = Math.PI / this.points;
+    var startAngle = 1.5 * Math.PI;
+    var margin = .25 * Math.PI / 180;
+
+    // Create slice clipping mask.
     this.sliceCtx.beginPath();
     this.sliceCtx.moveTo(0, 0);
     this.sliceCtx.arc(0, 0, this.radius, startAngle - this.arcLength / 2 - margin, startAngle + this.arcLength / 2 + margin, false);
@@ -50,18 +60,23 @@ Kaleidoscope.prototype.setPoints = function (points) {
 };
 
 Kaleidoscope.prototype.drawFrame = function (ts) {
+    // Get the current image rotation.
     var delta = ts - this.lastTs;
     this.lastTs = ts;
     this.rotatePercent += delta / this.rotateTime;
 
+    // Rotate the image and draw it onto the slice canvas.
     this.sliceCtx.rotate(this.rotatePercent * 2 * Math.PI);
     this.sliceCtx.drawImage(this.imageCanvas, -this.radius, -this.radius);
     this.sliceCtx.rotate(-this.rotatePercent * 2 * Math.PI);
 
+    // Draw non-mirrored slices.
     for (var i = 0; i < this.points; i++) {
         this.ctx.drawImage(this.sliceCanvas, -this.radius, -this.radius);
         this.ctx.rotate(this.arcLength * 2);
     }
+
+    // Draw mirrored slices.
     this.ctx.scale(-1, 1);
     this.ctx.rotate(this.arcLength);
     for (var _i = 0; _i < this.points; _i++) {
@@ -71,11 +86,13 @@ Kaleidoscope.prototype.drawFrame = function (ts) {
     this.ctx.scale(-1, 1);
     this.ctx.rotate(this.arcLength);
 
+    // Draw a border around the kaleidoscope to cover pixel artifacts.
     this.ctx.beginPath();
     this.ctx.strokeStyle = '#fff';
     this.ctx.lineWidth = 2;
     this.ctx.arc(0, 0, this.radius, 0, 2 * Math.PI);
     this.ctx.stroke();
 
+    // Request the next frame.
     window.requestAnimationFrame(this.drawFrame.bind(this));
 };
