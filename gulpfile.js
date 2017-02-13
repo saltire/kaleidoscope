968a0acc77876f9ea7c374b8fa38f77b219247a7
saltire sable <saltiresable@gmail.com>
2017-02-13 18:12:52 -0500

    Use Less

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,21 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const connect = require('gulp-connect');
+const less = require('gulp-less');
 const run = require('gulp-run');
 
 
 gulp.task('babel', () => {
     gulp.src('js/*.js')
         .pipe(babel({presets: ['es2015', 'react']}))
+        .on('error', (err) => console.log('Error parsing with Babel:', err.message))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('less', () => {
+    gulp.src('less/*.less')
+        .pipe(less())
+        .on('error', (err) => console.log('Error parsing LESS:', err.message))
         .pipe(gulp.dest('dist'));
 });
 
@@ -35,6 +44,7 @@ gulp.task('serve', () => {
 
 gulp.task('watch', () => {
     gulp.watch('js/*.js', ['babel']);
+    gulp.watch('less/*.less', ['less']);
     gulp.watch('static/*', ['static']);
 });
 
@@ -42,6 +52,6 @@ gulp.task('gh-pages', () => {
     run('git subtree push --prefix dist origin gh-pages').exec();
 });
 
-gulp.task('build', ['babel', 'libs', 'static']);
+gulp.task('build', ['babel', 'less', 'libs', 'static']);
 gulp.task('publish', ['build', 'gh-pages']);
 gulp.task('default', ['build', 'serve', 'watch']);
